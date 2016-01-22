48847326b596723729f91b743db6d46879042fd6
Jacob Koza <Jake@Jacobs-MacBook-Pro.local>
2016-01-22 11:18:23 -0500

    Initial commit

---
+++ app/src/main/java/kozagames/tester4/ScanQR.java
@@ -0,0 +1,19 @@
+package kozagames.tester4;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jake on 1/21/16.
+ */
+public class ScanQR extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.generate_workout, container, false);
+    }
+}
